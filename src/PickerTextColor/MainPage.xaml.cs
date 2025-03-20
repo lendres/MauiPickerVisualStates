0cdb294180dbbd963dadff39e98803e525ebf19f
Ikosy <lendres@fifthrace.com>
2025-03-20 06:42:24 -0500

    Add navigation

--- src/PickerTextColor/MainPage.xaml.cs
+++ src/PickerTextColor/MainPage.xaml.cs
@@ -8,5 +8,10 @@
 			MonkeyPicker1.SelectedIndex = 1;
 			MonkeyPicker2.SelectedIndex = 1;
         }
+
+		private void OnNavigateForward(object sender, EventArgs e)
+		{
+			Shell.Current.GoToAsync(nameof(SecondPage), true, new Dictionary<string, object> { });
+		}
     }
 }
---
+++ src/PickerTextColor/SecondPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PickerTextColor
+{
+	public partial class SecondPage : ContentPage
+	{
+		public SecondPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnNavigateBack(object sender, EventArgs eventArgs)
+		{
+			Shell.Current.GoToAsync("../");
+		}
+	}
+}
